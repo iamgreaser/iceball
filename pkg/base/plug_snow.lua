9e3af2e45eeb3ac2f3db1d8dbfacabfc24dd626a
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2012-12-16 22:08:32 +1300

    snow now gets added to the server over time

--- pkg/base/plug_snow.lua
+++ pkg/base/plug_snow.lua
@@ -15,34 +15,56 @@
     along with Ice Lua Components.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
-function snow_init_pissdown(p_snow)
-	print("Snowing the map...")
+
+function snow_drop_part(x,z,t,bcast)
 	local xlen,ylen,zlen
-	local x,y,z,i
 	xlen,ylen,zlen = common.map_get_dims()
 	
+	local ty = t[1+1]
+	if ty > 0 and ty < ylen-1 then
+		map_block_set(x,ty-1,z,2,255,255,255)
+		if img_overview then
+			common.img_pixel_set(img_overview,x,z,0xFFFFFFFF)
+		end
+		if bcast then
+			net_broadcast(nil, common.net_pack("BHHHBBBB",
+				0x08,x,ty-1,z,255,255,255,2))
+		end
+	end
+end
+
+function snow_drop(x,z,bcast)
+	local tl = common.map_pillar_get(x-1,z)
+	local tr = common.map_pillar_get(x+1,z)
+	local tu = common.map_pillar_get(x,z-1)
+	local td = common.map_pillar_get(x,z+1)
+	local tc = common.map_pillar_get(x,z)
+	
+	if tl[1+1] > tc[1+1] then
+		snow_drop_part(x-1,z,tl,bcast)
+	elseif tr[1+1] > tc[1+1] then
+		snow_drop_part(x+1,z,tr,bcast)
+	elseif tu[1+1] > tc[1+1] then
+		snow_drop_part(x,z-1,tu,bcast)
+	elseif td[1+1] > tc[1+1] then
+		snow_drop_part(x,z+1,td,bcast)
+	else
+		snow_drop_part(x,z,tc)
+	end
+end
+
+function snow_init_pissdown(p_snow)
+	local xlen,ylen,zlen
+	xlen,ylen,zlen = common.map_get_dims()
+	print("Snowing the map...",xlen,ylen,zlen)
+	local x,y,z,i
+	
 	for z=0,zlen-1 do
+	if z%8 == 0 then print(z) end
 	for x=0,xlen-1 do
-		local t = common.map_pillar_get(x,z)
-		--[[local s = ""
-		for i=1,#t do
-			s = s.." "..t[i]
+		if math.random() < p_snow then
+			snow_drop(x,z,false)
 		end
-		print(s)]]
-		if t[1+1] > 0 and t[1+1] < ylen-1 and math.random() < p_snow then
-			for i=#t,5,-1 do t[i+4] = t[i] end
-			if t[1+0] ~= 0 then t[1+0] = t[1+0] + 1 end
-			t[1+1] = t[1+1] - 1
-			t[5+0] = 255
-			t[5+1] = 255
-			t[5+2] = 255
-			t[5+3] = 2
-			if img_overview then
-				common.img_pixel_set(img_overview,x,z,0xFFFFFFFF)
-			end
-		end
-		--print(x,z)
-		common.map_pillar_set(x,z,t)
 	end
 	end
 	print("Done!")
@@ -87,8 +109,42 @@ function snow_init_hook()
 end
 
 if server then
-	snow_init_pissdown(0.1)
+	--snow_init_pissdown(0.1)
 	snow_init_hook()
+	local snow_lasttime = nil
+	local snow_freq = 0.1
+	local snow_oldtick = server.hook_tick
+	function snow_tick(sec_current, sec_delta)
+		snow_lasttime = snow_lasttime or sec_current
+		local ct = 5
+		local i
+		
+		-- hack to work around a bug
+		if snow_lasttime - sec_current > 5 then
+			snow_lasttime = sec_current
+		end
+		
+		while sec_current >= snow_lasttime + snow_freq do
+			local xlen,ylen,zlen
+			xlen,ylen,zlen = common.map_get_dims()
+			for i=1,5 do
+				snow_drop(math.floor(math.random()*xlen),math.floor(math.random()*zlen),true)
+			end
+			snow_lasttime = snow_lasttime + snow_freq
+			ct = ct - 1
+			if ct <= 0 then
+				snow_lasttime = sec_current
+				break
+			end
+		end
+		server.hook_tick = snow_oldtick
+		local ret = server.hook_tick(sec_current, sec_delta)
+		snow_oldtick = server.hook_tick
+		server.hook_tick = snow_tick
+		return ret
+	end
+	
+	server.hook_tick = snow_tick
 end
 
 if client then
