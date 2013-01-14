f1fb85ebb2cc59e2df4465fae580af41b63e5937
Dany0 <menko.dany425@gmail.com>
2013-01-14 01:27:05 +0100

    coded teleport and layed down snippet for goto
    learned a lot about how iceball works internally at that :P thanks to BR
    for pointing out the obvious!

--- pkg/base/main_server.lua
+++ pkg/base/main_server.lua
@@ -283,11 +283,21 @@ function server.hook_tick(sec_current, sec_delta)
 					end
 				elseif params[1] == "kill" then
 					plr.set_health_damage(0, 0xFF800000, plr.name.." shuffled off this mortal coil", plr)
+				elseif params[1] == "goto" then
+					if table.getn(params) == 2 then
+						--TODO: actually do the goto
+					else
+						net_broadcast(nil, common.net_pack("BIz", 0x0E, usage_colour, "Usage: /goto #X ; where # is letter, X is number in the map's grid system"))
+					end
 				elseif params[1] == "teleport" or params[1] == "tp" then
 					if table.getn(params) == 4 then
-						--TODO: Actually teleport player to (tonumber(params[2]), tonumber(params[3]), tonumber(params[4]))
+						--NOTE: I protest that y is down/same way AoS was
+						x, y, z = tonumber(params[2]), tonumber(params[3]), tonumber(params[4])
+						plr.set_pos_recv(x, y, z)
+						net_broadcast(sockfd, common.net_pack("BBhhh",
+							0x03, cli.plrid, x * 32.0, y * 32.0, z * 32.0))
 					else
-						net_broadcast(nil, common.net_pack("BIz", 0x0E, usage_colour, "Usage: /teleport x y z"))
+						net_broadcast(nil, common.net_pack("BIz", 0x0E, usage_colour, "Usage: /teleport x y z ; where y is down"))
 					end
 				end
 			else
