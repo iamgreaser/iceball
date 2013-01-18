6a1848cc11f33c9577ccbd9dee914f1d1047e164
triplefox <jhofmann@321f.net>
2013-01-18 15:16:32 -0800

    argb pack and unpack moved into c; update versioning

--- pkg/base/version.lua
+++ pkg/base/version.lua
@@ -16,9 +16,9 @@
 ]]
 
 VERSION_ENGINE = {
-	cmp={0,0,0,0,13},
-	num=13,
-	str="0.0-13",
+	cmp={0,0,0,0,14},
+	num=14,
+	str="0.0-14",
 }
 
 VERSION_BUGS = {
@@ -37,4 +37,5 @@ VERSION_BUGS = {
 {intro=nil, fix=12, msg="TGA loader prone to crashing on unsanitised data"},
 {intro=nil, fix=13, msg="No per-face shading"},
 {intro=nil, fix=nil, msg="Per-face shading is only preliminary"},
+{intro=nil, fix=14, msg="Color conversion functions are using hacky Lua code"},
 }
