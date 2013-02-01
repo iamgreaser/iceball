iamgreaser/iceball

commit b12f509d70a7409e7d7343ba9bf3bd0846fff78e

Changed how permissions are handed out Added permission helper functions to player "class"

pkg/base/obj_player.lua
@@ -52,6 +52,18 @@ function new_player(settings)
 	function this.remove_permission(perm)
 		this.permissions[perm] = nil
 	end
+	
+	function this.add_permission_group(perms)
+		for k,v in perms do
+			this.permissions[v] = true
+		end
+	end
+	
+	function this.remove_permission_group(perms)
+		for k,v in perms do
+			this.permissions[v] = nil
+		end
+	end
 
 	local function prv_recolor_team(r,g,b)
 		if not client then return end

pkg/base/commands.lua
@@ -216,8 +216,7 @@ command_register({
 		if table.getn(prms) == 2 then
 			local success = false
 			if permissions[prms[1]] ~= nil and prms[2] == permissions[prms[1]].password then
-				-- Should logging in change permissions or add to them? Should you be able to log out?
-				plr.permissions = permissions[prms[1]].perms
+				plr.add_permission_group(permissions[prms[1]].perms)
 				success = true
 			end
 			if success then

pkg/base/main_server.lua
@@ -53,7 +53,7 @@ function slot_add(sockfd, tidx, wpn, name)
 				pid = i,
 			})
 			if permissions["default"] ~= nil then
-				players[i].permissions = permissions["default"].perms
+				players[i].add_permission_group(permissions["default"].perms)
 				print("Adding default permissions for user")
 			else
 				print("Default permissions do not exist")

commit 5ac04277a4d2c1c00fea502125ba039d7a7e502e

Fixed permissions bug caused by b12f509d70a7409e7d7343ba9bf3bd0846fff78e

pkg/base/obj_player.lua
@@ -55,13 +55,13 @@ function new_player(settings)
 	
 	function this.add_permission_group(perms)
 		for k,v in pairs(perms) do
-			this.permissions[v] = true
+			this.add_permission(k)
 		end
 	end
 	
 	function this.remove_permission_group(perms)
 		for k,v in pairs(perms) do
-			this.permissions[v] = nil
+			this.remove_permission(k)
 		end
 	end
 