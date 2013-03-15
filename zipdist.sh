da7549be0023cbfd83a9445fe119a59428269b20
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2013-03-15 18:25:56 +1300

    nubdist for 0.0-29

--- zipdist.sh
+++ zipdist.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export ZIPNAME=nubdist/iceballfornoobs-0.0-27.zip
+export ZIPNAME=nubdist/iceballfornoobs-0.0-29.zip
 
 #zip -r $ZIPNAME *.dll *.exe *.txt *.bat docs/ \
 #	pkg/base/*.lua \
