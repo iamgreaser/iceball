9265a36c9a6e08e64ab93c88b19d69112f8eced7
10se1ucgo <hammaadu1@gmail.com>
2013-07-03 14:40:00 -0400

    Added PMF Editor Tutorial Added tutorial.bat and pmf_tutorial.bat Added tutorial/pmf editor tutorial to run-gl/run.bat Fixed broken parameter in run-gl/run.bat

--- run-gl.bat
+++ run-gl.bat
@@ -9,6 +9,8 @@ ECHO 3. Lighting test
 ECHO 4. Snow weather test
 ECHO 5. Map editor
 ECHO 6. PMF editor
+ECHO 7. Tutorial
+ECHO 8. PMF Editor Tutorial
 ECHO -------------------------------------
 ECHO 0. Exit
 ECHO -------------------------------------
@@ -24,6 +26,8 @@ IF /I '%INPUT%'=='3' GOTO LightingTest
 IF /I '%INPUT%'=='4' GOTO SnowTest
 IF /I '%INPUT%'=='5' GOTO MapEditor
 IF /I '%INPUT%'=='6' GOTO PMFEditor
+IF /I '%INPUT%'=='7' GOTO Tutorial
+IF /I '%INPUT%'=='8' GOTO PMFEditorTutorial
 IF /I '%INPUT%'=='0' EXIT
 
 ECHO Invalid option
@@ -40,7 +44,7 @@ GOTO Menu
 
 :DevServer
 ECHO Connecting to dev server...
-iceball-gl.exe -c iceballga.me 20737
+iceball-gl.exe -c play.iceballga.me 20737
 ECHO.
 cls
 GOTO Menu
@@ -69,6 +73,20 @@ GOTO Menu
 :PMFEditor
 ECHO Starting PMF editor...
 iceball-gl.exe -s 0 pkg/iceball/pmfedit
+ECHO.
+cls
+GOTO Menu
+
+:Tutorial
+ECHO Starting tutorial...
+iceball-gl.exe -s 0 pkg/iceball/halp
+ECHO.
+cls
+GOTO Menu
+
+:PMFEditorTutorial
+ECHO Starting tutorial...
+iceball-gl.exe -s 0 pkg/iceball/pmfedithalp
 ECHO.
 cls
 GOTO Menu
