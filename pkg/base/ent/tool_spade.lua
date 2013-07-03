42da4a33b59263ec8f756fa83cc7ef82b189e2b5
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2013-07-03 20:03:58 +1200

    this -> plr

--- pkg/base/ent/tool_spade.lua
+++ pkg/base/ent/tool_spade.lua
@@ -64,10 +64,10 @@ return function (plr)
 
 		if plr.tools[plr.tool+1] ~= this then return end
 
-		local sya = math.sin(this.angy)
-		local cya = math.cos(this.angy)
-		local sxa = math.sin(this.angx)
-		local cxa = math.cos(this.angx)
+		local sya = math.sin(plr.angy)
+		local cya = math.cos(plr.angy)
+		local sxa = math.sin(plr.angx)
+		local cxa = math.cos(plr.angx)
 		local fwx,fwy,fwz
 		fwx,fwy,fwz = sya*cxa, sxa, cya*cxa
 
