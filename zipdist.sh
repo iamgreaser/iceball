97d5ad219ab2e4f87185ac616530ca7c0ee856bd
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2013-07-31 20:09:03 +1200

    zipdist for 0.1.1-4

--- zipdist.sh
+++ zipdist.sh
@@ -1,21 +1,9 @@
 #!/bin/sh
 
-export ZIPNAME=nubdist/iceball-indev-0.1-9.zip
-
-#zip -r $ZIPNAME *.dll *.exe *.txt *.bat docs/ \
-#	pkg/base/*.lua \
-#	pkg/base/icegui/*.lua \
-#	pkg/maps/mesa.vxl \
-#	pkg/base/gfx/ pkg/base/pmf/ \
-#	pkg/iceball/snowtest/ \
-#	pkg/iceball/pmfedit/ \
-#	pkg/iceball/mapedit/ \
-#	dlcache/info.txt clsave/info.txt svsave/info.txt \
-#	clsave/config.json clsave/pub/user.json
+export ZIPNAME=nubdist/iceball-indev-0.1.1-4.zip
 
 zip -r $ZIPNAME *.dll *.exe *.txt opencmd.bat connect-*.bat docs/ \
 	dlcache/info.txt clsave/info.txt \
-	clsave/config.json clsave/pub/user.json \
-	pkg/iceball/halp/ \
-	clsave/pub/controls.json
+	clsave/config.json clsave/pub/user.json clsave/pub/controls.json \
+	pkg/iceball/halp/
 
