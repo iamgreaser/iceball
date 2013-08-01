2b03af049090093e4ad7fac7699831595a85bc3c
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2013-08-01 19:17:28 +1200

    zipdist for 0.1.1-8

--- zipdist.sh
+++ zipdist.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-export ZIPNAME=nubdist/iceball-indev-0.1.1-7.zip
+export ZIPNAME=nubdist/iceball-indev-0.1.1-8.zip
 
 zip -r $ZIPNAME *.dll *.exe *.txt opencmd.bat connect-*.bat docs/ \
 	dlcache/info.txt clsave/info.txt \
 	clsave/config.json clsave/pub/user.json clsave/pub/controls.json \
+	"DOUBLE CLICK ON THIS FILE TO GET THE MASTER SERVER LIST WORKING PROPERLY ON WINDOWS I MEAN IT.exe" \
 	pkg/iceball/halp/
 
