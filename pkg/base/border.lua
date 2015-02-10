d71caa4681117d043cbc86ea46153a6cf2c4df01
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2015-02-10 16:15:40 +1300

    alpha blended poly borders for LTS

--- pkg/base/border.lua
+++ pkg/base/border.lua
@@ -30,23 +30,63 @@ function new_border(x, y, z, nx, ny, nz, r, g, b)
 
 	-- generate model
 	if client then
-		this.mdl_border = common.model_new(1)
-		this.mdl_border, this.mdl_border_bone = common.model_bone_new(this.mdl_border, 1024)
+		if common.version.num >= 8421376+1 then
+			local i, j
+			local l = {}
 
-		local i, j
-		local l = {}
-		for i=-16,15 do
-		for j=-16,15 do
-			l[#l+1] = {
-				x = (this.ny*i + this.nz*j)*8,
-				y = (this.nz*i + this.nx*j)*8,
-				z = (this.nx*i + this.ny*j)*8,
-				r = this.r, g = this.g, b = this.b,
-				radius = 1,
-			}
+			local function point(i, j, u)
+				local v = (j/16+1)/2
+				return {
+					(this.ny*i + this.nz*j)*8,
+					(this.nz*i + this.nx*j)*8,
+					(this.nx*i + this.ny*j)*8,
+					r/255.0, g/255.0, b/255.0, (math.max(i*i, j*j)/256)*0.5+0.3,
+					u, v,
+				}
+			end
+			for i=-16,15,8 do
+			for j=-16,15,8 do
+				local p00 = point(i+0, j+0, 0)
+				local p01 = point(i+0, j+8, 0)
+				local p10 = point(i+8, j+0, 1)
+				local p11 = point(i+8, j+8, 1)
+
+				l[1+#l] = p00
+				l[1+#l] = p10
+				l[1+#l] = p01
+				l[1+#l] = p01
+				l[1+#l] = p10
+				l[1+#l] = p11
+
+				l[1+#l] = p11
+				l[1+#l] = p10
+				l[1+#l] = p01
+				l[1+#l] = p01
+				l[1+#l] = p10
+				l[1+#l] = p00
+			end
+			end
+
+			this.va_border = common.va_make(l, nil, "3v,4c,2t")
+		else
+			this.mdl_border = common.model_new(1)
+			this.mdl_border, this.mdl_border_bone = common.model_bone_new(this.mdl_border, 1024)
+
+			local i, j
+			local l = {}
+			for i=-16,15 do
+			for j=-16,15 do
+				l[#l+1] = {
+					x = (this.ny*i + this.nz*j)*8,
+					y = (this.nz*i + this.nx*j)*8,
+					z = (this.nx*i + this.ny*j)*8,
+					r = this.r, g = this.g, b = this.b,
+					radius = 1,
+				}
+			end
+			end
+			common.model_bone_set(this.mdl_border, this.mdl_border_bone, "border", l)
 		end
-		end
-		common.model_bone_set(this.mdl_border, this.mdl_border_bone, "border", l)
 	end
 
 	function this.tick(sec_current, sec_delta)
@@ -71,12 +111,22 @@ function new_border(x, y, z, nx, ny, nz, r, g, b)
 		cz = math.floor(cz/gran)*gran
 
 		local x, y, z
+		if this.va_border then
+			this.u = 0
+			this.v = 0
+		end
 		x = this.x*this.nx + cx*(1-this.nx) + this.ny*this.u + this.nz*this.v
 		y = this.y*this.ny + cy*(1-this.ny) + this.nz*this.u + this.nx*this.v
 		z = this.z*this.nz + cz*(1-this.nz) + this.nx*this.u + this.ny*this.v
 
-		client.model_render_bone_global(this.mdl_border, this.mdl_border_bone,
-			x, y, z, 0, 0, 0, 256)
+		if this.va_border then
+			client.va_render_global(this.va_border,
+				x, y, z, 0, 0, 0, 1,
+				nil, "ah")
+		else
+			client.model_render_bone_global(this.mdl_border, this.mdl_border_bone,
+				x, y, z, 0, 0, 0, 256)
+		end
 	end
 	
 	return this
