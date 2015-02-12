daeab9a4e9fdb75faa6d655e6d92a532af590b09
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2015-02-12 17:29:43 +1300

    stencil grass paint for rugby

--- pkg/iceball/rugby/main.lua
+++ pkg/iceball/rugby/main.lua
@@ -1,5 +1,58 @@
 do
 
+if client then
+	if not img_fsrect_wpaint then
+		img_fsrect_wpaint = client.img_new(client.screen_get_dims())
+		client.img_fill(img_fsrect_wpaint, 0x8FFFFFFF)
+	end
+
+	local xlen, ylen, zlen = common.map_get_dims()
+
+	local l = {}
+	function gen_box(t,x0,y0,z0,x1,y1,z1)
+		local i
+		local t2 = {
+			{x0,y0,z0, 0,0}, {x0,y1,z0, 0,0}, {x0,y0,z1, 0,0},
+			{x0,y0,z1, 0,0}, {x0,y1,z0, 0,0}, {x0,y1,z1, 0,0},
+
+			{x1,y1,z1, 0,0}, {x1,y1,z0, 0,0}, {x1,y0,z1, 0,0},
+			{x1,y0,z1, 0,0}, {x1,y1,z0, 0,0}, {x1,y0,z0, 0,0},
+
+			{x1,y1,z0, 0,0}, {x0,y1,z0, 0,0}, {x1,y0,z0, 0,0},
+			{x1,y0,z0, 0,0}, {x0,y1,z0, 0,0}, {x0,y0,z0, 0,0},
+			
+			{x0,y0,z1, 0,0}, {x0,y1,z1, 0,0}, {x1,y0,z1, 0,0},
+			{x1,y0,z1, 0,0}, {x0,y1,z1, 0,0}, {x1,y1,z1, 0,0},
+		}
+
+		for i=1,#t2 do
+			t[1+#t] = t2[i]
+		end
+	end
+	gen_box(l, 
+		xlen/2-0.2, 0, 15,
+		xlen/2+0.2, ylen, zlen-15)
+	gen_box(l, 
+		14.8, 0, 15,
+		15.2, ylen, zlen-15)
+	gen_box(l, 
+		xlen-15.2, 0, 15,
+		xlen-14.8, ylen, zlen-15)
+	gen_box(l, 
+		15, 0, 14.8,
+		xlen-15, ylen, 15.2)
+	gen_box(l, 
+		15, 0, zlen-15.2,
+		xlen-15, ylen, zlen-14.8)
+
+	va_fronts = common.va_make(l, nil, "3v,2t")
+	local i
+	for i=1,math.floor(#l/2) do
+		l[i], l[#l+1-i] = l[#l+1-i], l[i]
+	end
+	va_backs = common.va_make(l, nil, "3v,2t")
+end
+
 local s_new_intel = new_intel
 local function wrap_intel(this)
 	function this.get_name()
@@ -12,6 +65,46 @@ end
 function new_intel(settings, ...)
 	local this = s_new_intel(settings, ...)
 	return wrap_intel(this)
+end
+
+local s_new_player = new_player
+function new_player(...)
+	local this = s_new_player(...)
+
+	local s_show_hud = this.show_hud
+	function this.show_hud(sec_current, sec_delta, ...)
+		if client.gfx_stencil_test then
+			-- Stencil hackery
+			client.gfx_stencil_test(true)
+			--print("STENC")
+
+			-- PASS 1: increase for front faces, decrease for back faces
+			client.gfx_depth_mask(false)
+			client.gfx_alpha_test(false)
+			client.gfx_stencil_func(">", 255, 255)
+			client.gfx_stencil_op(";;+")
+			client.va_render_global(va_fronts,
+				0,0,0, 0,0,0, 1, nil, "01")
+			client.gfx_stencil_func("<", 0, 255)
+			client.gfx_stencil_op(";;-")
+			client.va_render_global(va_backs,
+				0,0,0, 0,0,0, 1, nil, "01")
+			client.gfx_alpha_test(true)
+			client.gfx_depth_mask(true)
+
+			-- PASS 2: draw white for stencil >= 1; clear stencil
+			client.gfx_stencil_func("<=", 1, 255)
+			client.gfx_stencil_op("000")
+			client.img_blit(img_fsrect_wpaint, 0, 0)
+
+			client.gfx_stencil_test(false)
+		end
+
+		-- continue
+		return s_show_hud(sec_current, sec_delta, ...)
+	end
+
+	return this
 end
 
 if miscents then
