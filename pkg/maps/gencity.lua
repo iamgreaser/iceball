b337889df02213bc7d1f0caab3a2d6a50aa3dbf6
LeComm <e290624@trbvm.com>
2015-02-14 11:42:43 +0100

    Another gencity update
    -Added roof fences
    -Streets are now created with a "snake algorithm", instead of random road cell generation
    -Simple basements are now created on uneven terrain (needs improvement and something to make cells connect properly)

--- pkg/maps/gencity.lua
+++ pkg/maps/gencity.lua
@@ -10,6 +10,7 @@ function BUILDING_WALL_COLOR() d=math.random(-4, 4) return {100+d, 100+d, 100+d}
 function BUILDING_FLOOR_COLOR() d=math.random(-4, 4) return {100+d, 100+d, 100+d} end
 function BUILDING_STAIR_COLUMN_COLOR() return {32, 32, 32} end
 function BUILDING_STAIR_COLOR() return {128, 128, 128} end
+function BUILDING_BASEMENT_COLOR() return {16, 16, 16} end
 
 function BUILDING_TABLE_COLOR() return {128, 64, 0} end
 TABLE_W=2
@@ -34,6 +35,7 @@ zcells=32
 
 cells={}
 celldata={}
+cellheights={}
 
 MIN_BUILDING_H=2
 MAX_BUILDING_H=4
@@ -123,12 +125,48 @@ function gen_lantern(x1, y1, z1, xd, zd)
 	map_block_set(x1+xd*3, hy, z1+zd*3, 1, unpack(LANTERN_COLOR()))
 end
 
-function gen_grass(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
+function create_building_site(x1, z1, x2, z2)
+	xlen, ylen, zlen=common.map_get_dims()
+	y1=map_highest_point(x1, 0, z1, x2, ylen, z2)
+	for x=x1, x2-1 do
+		for z=z1, z2-1 do
+			y2=map_height_get(x, z)
+			for y=y1, y2 do
+				map_block_set(x, y, z, 1, unpack(CONCRETE_GROUND_COLOR()))
+			end
+		end
+	end
+	return y1
+end
+
+function map_highest_point(x1, y1, z1, x2, y2, z2)
+	highest_point=y2
+	for x=x1, x2-1 do
+		for z=z1, z2-1 do
+			y=map_height_get(x, z, y1)
+			if y<highest_point then highest_point=y end
+		end
+	end
+	return highest_point
+end
+
+function map_height_get(x, z, y1)
+	if y1==nil then y1=0 end
+	xlen, ylen, zlen=common.map_get_dims()
+	for y=y1, ylen do
+		if map_block_get(x, y, z) then
+			return y
+		end
+	end
+	return y
+end
+
+function gen_grass(x1, z1, lower_y, xsize, zsize, xnl, xnr, znl, znr)
 	x2=x1+xsize
 	z2=z1+zsize
 	for x=0, xsize-1 do
 		for z=0, zsize-1 do
-			map_block_set(x+x1, FLOOR_Y, z+z1, 1, unpack(GRASS_COLOR()))
+			map_block_set(x+x1, lower_y, z+z1, 1, unpack(GRASS_COLOR()))
 		end
 	end
 	wallxd=0
@@ -147,7 +185,7 @@ function gen_grass(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
 				mdl=1-mdl
 				for y=0, FENCE_HEIGHT do
 					if(y%2)==mdl then
-						map_block_set(fencex, FLOOR_Y-y, z, 1, unpack(FENCE_COLOR()))
+						map_block_set(fencex, lower_y-y, z, 1, unpack(FENCE_COLOR()))
 					end
 				end
 			end
@@ -157,7 +195,7 @@ function gen_grass(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
 				mdl=1-mdl
 				for y=0, FENCE_HEIGHT do
 					if(y%2)==mdl then
-						map_block_set(x, FLOOR_Y-y, fencez, 1,  unpack(FENCE_COLOR()))
+						map_block_set(x, lower_y-y, fencez, 1,  unpack(FENCE_COLOR()))
 					end
 				end
 			end
@@ -173,7 +211,7 @@ function gen_grass(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
 				x=x1+math.random(1, xsize-1)
 				z=z1+zsize/2+(zsize/2-math.random(1, 2)-ternary(wallzd==1, 1, 0))*wallzd
 			end
-			gen_bin(x, FLOOR_Y-1, z)
+			gen_bin(x, lower_y-1, z)
 		end
 	end
 	bushes=math.random(0, 5)
@@ -183,13 +221,13 @@ function gen_grass(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
 		for b=0, bushes-1 do
 			x=math.random(x1+4, x2-bush_l-4)
 			z=math.random(z1+4, z2-bush_w-4)
-			gen_bush(x, FLOOR_Y-bush_w, z, x+bush_l, FLOOR_Y, z+bush_w)
+			gen_bush(x, lower_y-bush_w, z, x+bush_l, lower_y, z+bush_w)
 		end
 	end
 end
 
 --Array accesses need to be replaced by arrays passed to functions
-function gen_building(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
+function gen_building(x1, z1, lower_y, xsize, zsize, xnl, xnr, znl, znr)
 	x2=x1+xsize-1
 	z2=z1+zsize-1
 	xc=x1/xsize
@@ -209,10 +247,9 @@ function gen_building(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
 	do_stairs=neighbour_higher==false or math.random(0, 3)==0
 	midx=xsize/2+x1
 	midz=zsize/2+z1
-	--walls
 	for f=0, h-1 do
-		y1=FLOOR_Y-(f+1)*BUILDING_FLOOR_HEIGHT+1
-		y2=FLOOR_Y-f*BUILDING_FLOOR_HEIGHT-1
+		y1=lower_y-(f+1)*BUILDING_FLOOR_HEIGHT+1
+		y2=lower_y-f*BUILDING_FLOOR_HEIGHT-1
 		fy=y1-1
 		--The floor (+ hole at stair positions)
 		for x=x1, x2 do
@@ -313,45 +350,13 @@ function gen_building(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
 				window_c=window_c+1
 			end
 		end
-		--Doors
-		--[[
-		DOOR_WIDTH=10
-		if zlh==f or zrh==f then
-			xs=x1+(xsize-DOOR_WIDTH)/2
-			xe=x2-(xsize-DOOR_WIDTH)/2
-			for x=xs, xe do
-				for y=y1, y2 do
-					if zlh==f then
-						map_block_break(x, y, z1)
-					end
-					if zrh==f then
-						map_block_break(x, y, z2)
-					end
-				end
-			end
-		end
-		if xlh==f or xrh==f then
-			zs=z1+(zsize-DOOR_WIDTH)/2
-			ze=z2-(zsize-DOOR_WIDTH)/2
-			for z=zs, ze do
-				for y=y1, y2 do
-					if xlh==f then
-						map_block_break(x1, y, z)
-					end
-					if xrh==f then
-						map_block_break(x2, y, z)
-					end
-				end
-			end
-		end
-		--]]
 		--Stairs
 		if do_stairs then
-			for y=FLOOR_Y-(f+1)*BUILDING_FLOOR_HEIGHT, FLOOR_Y-f*BUILDING_FLOOR_HEIGHT do
+			for y=lower_y-(f+1)*BUILDING_FLOOR_HEIGHT, lower_y-f*BUILDING_FLOOR_HEIGHT do
 				x=midx
 				z=midz
 				map_block_set(x, y, z, 1, unpack(BUILDING_STAIR_COLUMN_COLOR()))
-				ymod=(y-FLOOR_Y+1)%8
+				ymod=(y-lower_y+1)%8
 				if ymod>=1 and ymod<4 then
 					x=x+1
 				else
@@ -359,7 +364,7 @@ function gen_building(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
 						x=x-1
 					end
 				end
-				ymod=(y-FLOOR_Y-1)%8
+				ymod=(y-lower_y-1)%8
 				if ymod>=1 and ymod<4 then
 					z=z+1
 				else
@@ -371,99 +376,98 @@ function gen_building(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
 			end
 		end
 	end
+	--Roof
+	do_xlr=xnl~=false and xlh<h
+	do_xrr=xnr~=false and xrh<h
+	do_zlr=znl~=false and zlh<h
+	do_zrr=znr~=false and zrh<h
+	if do_xlhw or do_xrhw then
+		for z=z1, z2 do
+			if do_xlhw then
+				map_block_set(x1, lower_y-h*BUILDING_FLOOR_HEIGHT-1, z, 1, unpack(BUILDING_WALL_COLOR()))
+			end
+			if do_xrhw then
+				map_block_set(x2, lower_y-h*BUILDING_FLOOR_HEIGHT-1, z, 1, unpack(BUILDING_WALL_COLOR()))
+			end
+		end
+	end
+	if do_zlhw or do_zrhw then
+		for x=x1, x2 do
+			if do_zlhw then
+				map_block_set(x, lower_y-h*BUILDING_FLOOR_HEIGHT-1, z1, 1, unpack(BUILDING_WALL_COLOR()))
+			end
+			if do_zrhw then
+				map_block_set(x, lower_y-h*BUILDING_FLOOR_HEIGHT-1, z2, 1, unpack(BUILDING_WALL_COLOR()))
+			end
+		end
+	end
 end
 
 -- xc = grid x ; zc = grid z ; xnl, xnr = left/right neighbour tiles ; znl, znr = upper/lower neighbour tiles
-function gen_road(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
+function gen_road(x1, z1, lower_y, xsize, zsize, xnl, xnr, znl, znr)
 	x2=x1+xsize
 	z2=z1+zsize
 	white_r=255
 	white_g=255
 	white_b=255
 	powrange=(ROAD_W/2.0)*(ROAD_W/2.0)
+	cx=x1/xsize
+	cz=z1/zsize
+	lxyf=0
+	rxyf=0
+	lzyf=0
+	rzyf=0
+	if xnl==gen_road then lxyf=(cellheights[cx-1][cz]-cellheights[cx][cz])/(xsize/2) end
+	if xnr==gen_road then rxyf=(cellheights[cx+1][cz]-cellheights[cx][cz])/(xsize/2) end
+	if znl==gen_road then lzyf=(cellheights[cx][cz-1]-cellheights[cx][cz])/(xsize/2) end
+	if znr==gen_road then rzyf=(cellheights[cx][cz+1]-cellheights[cx][cz])/(xsize/2) end
 	for x=0, xsize-1 do
 		for z=0, zsize-1 do
 			if (x+.5-xsize/2.0)*(x+.5-xsize/2.0)+(z+.5-zsize/2.0)*(z+.5-zsize/2.0)<=powrange then
-				map_block_set(x+x1, FLOOR_Y, z+z1, 1, unpack(ROAD_COLOR()))
+				map_block_set(x+x1, lower_y, z+z1, 1, unpack(ROAD_COLOR()))
 			end
 		end
 	end
-	if znl==gen_road then
+	if znl==gen_road or znr==gen_road then
 		midx=x1+xsize/2
-		for z=z1, z1+zsize/2 do
+		sz=z1+ternary(znl~=gen_road, xsize/2, 0)
+		ez=z1+zsize/2+ternary(znr==gen_road, xsize/2, 0)
+		for z=sz, ez-1 do
 			build_white_line=(z%4)~=0
 			for x=midx-HROAD_W, midx+HROAD_W-1 do
 				continue=0
-				c=map_block_get(x, FLOOR_Y, z)
+				c=map_block_get(x, lower_y, z)
 				if c[2]==white_r and c[3]==white_g and c[4]==white_b then
 					continue=1
 				end
 				if (x==midx) and build_white_line and (continue==0) then
-					map_block_set(x, FLOOR_Y, z, 1, white_r, white_g, white_b)
+					map_block_set(x, lower_y, z, 1, white_r, white_g, white_b)
 					continue=1
 				end
 				if continue==0 then
-					map_block_set(x, FLOOR_Y, z, 1, unpack(ROAD_COLOR()))
+					map_block_set(x, lower_y, z, 1, unpack(ROAD_COLOR()))
 				end
 			end
 		end
 	end
-	if znr==gen_road then
-		midx=x1+xsize/2
-		for z=z1+zsize/2, z2 do
-			build_white_line=(z%4)~=0
-			for x=midx-HROAD_W, midx+HROAD_W-1 do
-				continue=0
-				c=map_block_get(x, FLOOR_Y, z)
-				if c[2]==white_r and c[3]==white_g and c[4]==white_b then
-					continue=1
-				end
-				if (x==midx) and build_white_line and (continue==0) then
-					map_block_set(x, FLOOR_Y, z, 1, white_r, white_g, white_b)
-					continue=1
-				end
-				if continue==0 then
-					map_block_set(x, FLOOR_Y, z, 1, unpack(ROAD_COLOR()))
-				end
-			end
-		end
-	end
-	if xnl==gen_road then
+	if xnl==gen_road or xnr==gen_road then
 		midz=z1+zsize/2
-		for x=x1, x1+xsize/2 do
+		sx=x1+ternary(xnl~=gen_road, xsize/2, 0)
+		ex=x1+xsize/2+ternary(xnr==gen_road, xsize/2, 0)
+		for x=sx, ex-1 do
 			build_white_line=(x%4)~=0
 			for z=midz-HROAD_W, midz+HROAD_W-1 do
 				continue=0
-				c=map_block_get(x, FLOOR_Y, z)
+				c=map_block_get(x, lower_y, z)
 				if c[2]==white_r and c[3]==white_g and c[4]==white_b then
 					continue=1
 				end
 				if (z==midz) and build_white_line and continue==0 then
-					map_block_set(x, FLOOR_Y, z, 1, white_r, white_g, white_b)
+					map_block_set(x, lower_y, z, 1, white_r, white_g, white_b)
 					continue=1
 				end
 				if continue==0 then
-					map_block_set(x, FLOOR_Y, z, 1, unpack(ROAD_COLOR()))
-				end
-			end
-		end
-	end
-	if xnr==gen_road then
-		midz=z1+zsize/2
-		for x=x1+xsize/2, x2 do
-			build_white_line=(x%4)~=0
-			for z=midz-HROAD_W, midz+HROAD_W-1 do
-				continue=0
-				c=map_block_get(x, FLOOR_Y, z)
-				if c[2]==white_r and c[3]==white_g and c[4]==white_b then
-					continue=1
-				end
-				if (z==midz) and build_white_line and (continue==0) then
-					map_block_set(x, FLOOR_Y, z, 1, white_r, white_g, white_b)
-					continue=1
-				end
-				if continue==0 then
-					map_block_set(x, FLOOR_Y, z, 1, unpack(ROAD_COLOR()))
+					map_block_set(x, lower_y, z, 1, unpack(ROAD_COLOR()))
 				end
 			end
 		end
@@ -476,13 +480,13 @@ function gen_road(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
 				zd=math.random(0, 1)*2-1
 				x=math.random(x1, x2-1)
 				z=z1+zsize/2+(zsize/2-ternary(zd==1, 1, 0))*zd
-				gen_bin(x, FLOOR_Y-1, z)
+				gen_bin(x, lower_y-1, z)
 			end
 		end
 		if xnr==gen_road or xnl==gen_road then
 			for x=x1+LANTERN_INTERVAL*ternary(xnl~=gen_road, 1, 0),  x2-1-LANTERN_INTERVAL*ternary(xnr~=gen_road, 1, 0), LANTERN_INTERVAL do
-				gen_lantern(x, FLOOR_Y-1, z1+1, 0, 1)
-				gen_lantern(x, FLOOR_Y-1, z2-2, 0, -1)
+				gen_lantern(x, lower_y-1, z1+1, 0, 1)
+				gen_lantern(x, lower_y-1, z2-2, 0, -1)
 			end
 		end
 	end
@@ -494,36 +498,66 @@ function gen_road(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
 				xd=math.random(0, 1)*2-1
 				z=math.random(x1, x2-1)
 				x=x1+xsize/2+(xsize/2-ternary(wallxd==1, 1, 0))*xd
-				gen_bin(x, FLOOR_Y-1, z)
+				gen_bin(x, lower_y-1, z)
 			end
 		end
 		if znr==gen_road or znl==gen_road then
 			for z=z1+LANTERN_INTERVAL*ternary(znl~=gen_road, 1, 0),  z2-1-LANTERN_INTERVAL*ternary(znr~=gen_road, 1, 0), LANTERN_INTERVAL do
-				gen_lantern(x1+1, FLOOR_Y-1, z, 1, 0)
-				gen_lantern(x2-2, FLOOR_Y-1, z, -1, 0)
+				gen_lantern(x1+1, lower_y-1, z, 1, 0)
+				gen_lantern(x2-2, lower_y-1, z, -1, 0)
 			end
 		end
 	end
 end
 
-function gen_border(x1, z1, xsize, zsize, xnl, xnr, znl, znr)
+function gen_border(x1, z1, lower_y, xsize, zsize, xnl, xnr, znl, znr)
 	for x=0, xsize-1 do
 		for z=0, zsize-1 do
-			map_block_set(x+x1, FLOOR_Y, z+z1, 1, unpack(GRASS_COLOR()))
+			map_block_set(x+x1, lower_y, z+z1, 1, unpack(GRASS_COLOR()))
 		end
 	end
 end
 
 
-gen_funcs={gen_building, gen_grass, gen_road}
-gen_funcs_count=3
+gen_funcs={gen_building, gen_grass}
 
 function choice(array)
 	return array[math.random(#array)]
 end
 
+--Needs to be improved
+function road_snake(z)
+	x=0
+	zd=0
+	l=math.random(xcells, math.floor(xcells*1.5))
+	for i=0, l do
+		cells[x][z]=gen_road
+		z=z+zd
+		x=x+ternary(zd==0, 1, 0)
+		if x<0 or x>=xcells or z<0 or z>=zcells then break end
+		if math.random(0, 3)==0 then
+			zd=math.random(-1, 1)
+		end
+		--Avoid parallel roads (90° crossings are allowed!)
+		if z<zcells-1 then
+			if zd~=-1 and cells[x][z+1]==gen_road then zd=zd-1 end
+		end
+		if z>0 then
+			if zd~=1 and cells[x][z-1]==gen_road then zd=zd+1 end
+		end
+		if x>0 and z+zd>0 and z+zd<zcells-1 then
+			if zd~=0 and cells[x-1][z+zd]==gen_road then zd=0 end
+		end
+		if x<xcells-1 and z+zd>0 and z+zd<zcells-1 then
+			if zd~=0 and cells[x+1][z+zd]==gen_road then zd=0 end
+		end
+	end
+	
+end
+
 do
 	local loose, user_toggles, user_settings
+	local require=require
 	loose, user_toggles, user_settings = ...
 	local mx=user_settings["mx"] or 512
 	local my=user_settings["my"] or 96
@@ -535,21 +569,28 @@ do
 	FLOOR_Y=my-4
 	xcsize=mx/xcells
 	zcsize=mz/zcells
+	h=4
 	for x=0, mx-1 do
 		for z=0, mz-1 do
 			c=CONCRETE_GROUND_COLOR()
-			l={0, my-4, my-4, 0, c[1], c[2], c[3], 1}
+			l={0, my-h, my-h, 0, c[1], c[2], c[3], 1}
 			common.map_pillar_set(x, z, l)
 		end
 	end
+	map_cache_start()
 	for x=0, xcells-1 do
 		cells[x]={}
 		celldata[x]={}
+		cellheights[x]={}
 		xborder=x==0 or x==xcells-1
 		for z=0, zcells-1 do
+			cellheights[x][z]=create_building_site(x*xcsize, z*zcsize, (x+1)*xcsize, (z+1)*zcsize)
+			--cellheights[x][z]=my-4			--replace with this line to make it 100x faster (will disable support for uneven terrain)
 			zborder=z==0 or z==zcells-1
 			if (xborder==false) and (zborder==false) then
 				cell=choice(gen_funcs)
+				--TODO: make different cells spawn with different amounts
+				if math.random(0, 5)==0 then cell=gen_road end
 				cells[x][z]=cell
 				if cell==gen_building then
 					celldata[x][z]=math.random(MIN_BUILDING_H, MAX_BUILDING_H)
@@ -559,26 +600,13 @@ do
 			end
 		end
 	end
-	map_cache_start()
-	z=math.random(1, zcells-2)
-	zd=0
-	for x=0, xcells-1 do
-		if not zd then
-			zd=math.random(-1, 1)
-			z=z+zd
-			if z>=zcells then
-				z=0
-			end
-			if z<0 then
-				z=zcells-1
-			end
-		else
-			zd=0
-		end
-		cells[x][z]=gen_road
-		if zd and x~=0 then
-			cells[x-1][z]=gen_road
-		end
+	rc=math.floor((mx*mx+mz*mz)/65536)
+	rnd=math.floor(rc/2)
+	if rnd>0 then
+		rc=rc+math.random(-rnd, rnd)
+	end
+	for i=0, rc do
+		road_snake(math.random(1, zcells-2))
 	end
 	for x=0, xcells-1 do
 		for z=0, zcells-1 do
@@ -598,8 +626,9 @@ do
 			if z<=zcells-2 then
 				znr=cells[x][z+1]
 			end
-			cells[x][z](x*xcsize, z*zcsize, xcsize, zcsize, xnl, xnr, znl, znr)
+			cells[x][z](x*xcsize, z*zcsize, cellheights[x][z], xcsize, zcsize, xnl, xnr, znl, znr)
 		end
+		print(x*100/(xcells-1).."% generated")
 	end
 	map_cache_end()
 	return ret, "lecom_gencity("..mx..","..mz..","..my..")"
