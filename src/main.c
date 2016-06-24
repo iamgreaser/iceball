862072eb50e68f502a476a4e2fb6ad551a74bc3f
Rukachan <rukachan@users.noreply.github.com>
2016-06-24 19:04:50 +0300

    Use SDL_GetKeyFromScancode instead of remap_scancodes. Remove a cast from NULL.

--- src/main.c
+++ src/main.c
@@ -247,123 +247,7 @@ void platform_deinit(void)
 {
 	SDL_Quit();
 }
-
-int remap_scancodes(int scancode) {
-	switch (scancode) {
-		case SDL_SCANCODE_UNKNOWN: return SDLK_UNKNOWN;
-		case SDL_SCANCODE_A: return SDLK_a;
-		case SDL_SCANCODE_B: return SDLK_b;
-		case SDL_SCANCODE_C: return SDLK_c;
-		case SDL_SCANCODE_D: return SDLK_d;
-		case SDL_SCANCODE_E: return SDLK_e;
-		case SDL_SCANCODE_F: return SDLK_f;
-		case SDL_SCANCODE_G: return SDLK_g;
-		case SDL_SCANCODE_H: return SDLK_h;
-		case SDL_SCANCODE_I: return SDLK_i;
-		case SDL_SCANCODE_J: return SDLK_j;
-		case SDL_SCANCODE_K: return SDLK_k;
-		case SDL_SCANCODE_L: return SDLK_l;
-		case SDL_SCANCODE_M: return SDLK_m;
-		case SDL_SCANCODE_N: return SDLK_n;
-		case SDL_SCANCODE_O: return SDLK_o;
-		case SDL_SCANCODE_P: return SDLK_p;
-		case SDL_SCANCODE_Q: return SDLK_q;
-		case SDL_SCANCODE_R: return SDLK_r;
-		case SDL_SCANCODE_S: return SDLK_s;
-		case SDL_SCANCODE_T: return SDLK_t;
-		case SDL_SCANCODE_U: return SDLK_u;
-		case SDL_SCANCODE_V: return SDLK_v;
-		case SDL_SCANCODE_W: return SDLK_w;
-		case SDL_SCANCODE_X: return SDLK_x;
-		case SDL_SCANCODE_Y: return SDLK_y;
-		case SDL_SCANCODE_Z: return SDLK_z;
-		case SDL_SCANCODE_1: return SDLK_1;
-		case SDL_SCANCODE_2: return SDLK_2;
-		case SDL_SCANCODE_3: return SDLK_3;
-		case SDL_SCANCODE_4: return SDLK_4;
-		case SDL_SCANCODE_5: return SDLK_5;
-		case SDL_SCANCODE_6: return SDLK_6;
-		case SDL_SCANCODE_7: return SDLK_7;
-		case SDL_SCANCODE_8: return SDLK_8;
-		case SDL_SCANCODE_9: return SDLK_9;
-		case SDL_SCANCODE_0: return SDLK_0;
-		case SDL_SCANCODE_RETURN: return SDLK_RETURN;
-		case SDL_SCANCODE_ESCAPE: return SDLK_ESCAPE;
-		case SDL_SCANCODE_BACKSPACE: return SDLK_BACKSPACE;
-		case SDL_SCANCODE_TAB: return SDLK_TAB;
-		case SDL_SCANCODE_SPACE: return SDLK_SPACE;
-		case SDL_SCANCODE_MINUS: return SDLK_MINUS;
-		case SDL_SCANCODE_EQUALS: return SDLK_EQUALS;
-		case SDL_SCANCODE_LEFTBRACKET: return SDLK_LEFTBRACKET;
-		case SDL_SCANCODE_RIGHTBRACKET: return SDLK_RIGHTBRACKET;
-		case SDL_SCANCODE_BACKSLASH: return SDLK_BACKSLASH;
-		case SDL_SCANCODE_SEMICOLON: return SDLK_SEMICOLON;
-		case SDL_SCANCODE_COMMA: return SDLK_COMMA;
-		case SDL_SCANCODE_PERIOD: return SDLK_PERIOD;
-		case SDL_SCANCODE_SLASH: return SDLK_SLASH;
-		case SDL_SCANCODE_CAPSLOCK: return 301 /* SDLK_CAPSLOCK */;
-		case SDL_SCANCODE_F1: return 282 /* SDLK_F1 */;
-		case SDL_SCANCODE_F2: return 283 /* SDLK_F2 */;
-		case SDL_SCANCODE_F3: return 284 /* SDLK_F3 */;
-		case SDL_SCANCODE_F4: return 285 /* SDLK_F4 */;
-		case SDL_SCANCODE_F5: return 286 /* SDLK_F5 */;
-		case SDL_SCANCODE_F6: return 287 /* SDLK_F6 */;
-		case SDL_SCANCODE_F7: return 288 /* SDLK_F7 */;
-		case SDL_SCANCODE_F8: return 289 /* SDLK_F8 */;
-		case SDL_SCANCODE_F9: return 290 /* SDLK_F9 */;
-		case SDL_SCANCODE_F10: return 291 /* SDLK_F10 */;
-		case SDL_SCANCODE_F11: return 292 /* SDLK_F11 */;
-		case SDL_SCANCODE_F12: return 293 /* SDLK_F12 */;
-		case SDL_SCANCODE_F13: return 294 /* SDLK_F13 */;
-		case SDL_SCANCODE_F14: return 295 /* SDLK_F14 */;
-		case SDL_SCANCODE_F15: return 296 /* SDLK_F15 */;
-		case SDL_SCANCODE_SCROLLLOCK: return 302 /* SDLK_SCROLLLOCK */;
-		case SDL_SCANCODE_PAUSE: return 19 /* SDLK_PAUSE */;
-		case SDL_SCANCODE_INSERT: return 277 /* SDLK_INSERT */;
-		case SDL_SCANCODE_HOME: return 278 /* SDLK_HOME */;
-		case SDL_SCANCODE_PAGEUP: return 280 /* SDLK_PAGEUP */;
-		case SDL_SCANCODE_DELETE: return 127 /* SDLK_DELETE */;
-		case SDL_SCANCODE_END: return 279 /* SDLK_END */;
-		case SDL_SCANCODE_PAGEDOWN: return 281 /* SDLK_PAGEDOWN */;
-		case SDL_SCANCODE_RIGHT: return 275 /* SDLK_RIGHT */;
-		case SDL_SCANCODE_LEFT: return 276 /* SDLK_LEFT */;
-		case SDL_SCANCODE_DOWN: return 274 /* SDLK_DOWN */;
-		case SDL_SCANCODE_UP: return 273 /* SDLK_UP */;
-		case SDL_SCANCODE_KP_DIVIDE: return 267 /* SDLK_KP_DIVIDE */;
-		case SDL_SCANCODE_KP_MULTIPLY: return 268 /* SDLK_KP_MULTIPLY */;
-		case SDL_SCANCODE_KP_MINUS: return 269 /* SDLK_KP_MINUS */;
-		case SDL_SCANCODE_KP_PLUS: return 270 /* SDLK_KP_PLUS */;
-		case SDL_SCANCODE_KP_ENTER: return 271 /* SDLK_KP_ENTER */;
-		case SDL_SCANCODE_KP_1: return 257 /* SDLK_KP_1 */;
-		case SDL_SCANCODE_KP_2: return 258 /* SDLK_KP_2 */;
-		case SDL_SCANCODE_KP_3: return 259 /* SDLK_KP_3 */;
-		case SDL_SCANCODE_KP_4: return 260 /* SDLK_KP_4 */;
-		case SDL_SCANCODE_KP_5: return 261 /* SDLK_KP_5 */;
-		case SDL_SCANCODE_KP_6: return 262 /* SDLK_KP_6 */;
-		case SDL_SCANCODE_KP_7: return 263 /* SDLK_KP_7 */;
-		case SDL_SCANCODE_KP_8: return 264 /* SDLK_KP_8 */;
-		case SDL_SCANCODE_KP_9: return 265 /* SDLK_KP_9 */;
-		case SDL_SCANCODE_KP_0: return 256 /* SDLK_KP_0 */;
-		case SDL_SCANCODE_KP_PERIOD: return 266 /* SDLK_KP_PERIOD */;
-		case SDL_SCANCODE_POWER: return 320 /* SDLK_POWER */;
-		case SDL_SCANCODE_KP_EQUALS: return 272 /* SDLK_KP_EQUALS */;
-		case SDL_SCANCODE_SYSREQ: return 317 /* SDLK_SYSREQ */;
-		case SDL_SCANCODE_HELP: return 315 /* SDLK_HELP */;
-		case SDL_SCANCODE_MENU: return 319 /* SDLK_MENU */;
-		case SDL_SCANCODE_UNDO: return 322 /* SDLK_UNDO */;
-		case SDL_SCANCODE_LCTRL: return 306 /* SDLK_LCTRL */;
-		case SDL_SCANCODE_LSHIFT: return 304 /* SDLK_LSHIFT */;
-		case SDL_SCANCODE_LALT: return 308 /* SDLK_LALT */;
-		case SDL_SCANCODE_LGUI: return 310 /* SDLK_LGUI */;
-		case SDL_SCANCODE_RCTRL: return 305 /* SDLK_RCTRL */;
-		case SDL_SCANCODE_RSHIFT: return 303 /* SDLK_RSHIFT */;
-		case SDL_SCANCODE_RALT: return 307 /* SDLK_RALT */;
-		case SDL_SCANCODE_RGUI: return 309 /* SDLK_RGUI */;
-		case SDL_SCANCODE_MODE: return 313 /* SDLK_MODE */;
-		default: return -1;
-	}
-}
-#endif
+#endif /* ifndef DEDI */
 
 #if defined(DEDI) && defined(WIN32)
 int64_t ms_now()
@@ -459,9 +343,7 @@ static int ib_client_key_hook(SDL_Event ev) {
 		return 0;
 	}
 
-	int ch = remap_scancodes(ev.key.keysym.scancode);
-	//if ((ev.key.keysym.unicode & 0xFF80) == 0)
-	//	ch = ev.key.keysym.unicode & 0x1FF;
+	int ch = SDL_GetKeyFromScancode(ev.key.keysym.scancode);
 
 	lua_pushinteger(lstate_client, ch);
 	lua_pushboolean(lstate_client, (ev.type == SDL_KEYDOWN));
@@ -633,10 +515,8 @@ int render_client(void)
 	render_clear(&tcam);
 	if(map_enable_autorender)
 	{
-		render_cubemap((uint32_t*)NULL,
-			screen_width, screen_height, 0/4,
-			&tcam, clmap,
-			NULL, 0, '1', '0', 1.0f, 0);
+		render_cubemap(NULL, screen_width, screen_height, 0/4, &tcam,
+			       clmap, NULL, 0, '1', '0', 1.0f, 0);
 	}
 
 	// apply Lua HUD / model stuff
